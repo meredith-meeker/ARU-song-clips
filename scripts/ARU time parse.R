b782ac79b6770d2d00636c6cec596c18e88eb2f7
meredith-meeker <mmeeker118@gmail.com>
2022-08-25 13:10:25 -0400

    Meredith's attempt at selecting times

--- scripts/ARU time parse.R
+++ scripts/ARU time parse.R
@@ -1,30 +1,37 @@
 library(tidyverse)
 library(tuneR)
-
+library(lubridate)
+library(dplyr)
+library(tuneR)
 
 load("main_wav_list_SWMP_orange.RData")
 
 
-wav_name <- function(d = "20220617",
-                     tm = "07:00", # 24 hour time
-                     s = "RM22699",
+wav_name <- function(d = "20220618",
+                     tm = "05:17:00", # 24 hour time
+                     s = "RM22668",
                      dflist = list_files_wetlands){
   
   require(lubridate)
   
   # time selected as a proper lubridate oject
-  t = paste()# turn the true time into a character
   
+  tm <- lubridate::hms(tm) # turn the true time into a character
+
+    # vector of recording start times at that site and day
   
-  # vector of recording start times at that site and day
-  start_times <- list_files_wetlands[which(dflist[,"date"] == d & dflist[,"siteID"] == s),"time"]
-  
-  # identify which of the start times contains the selected time
-  
-  
-  # identify  the number of seconds between the start time and the selected time
-  dif_sec <- 
+  tm2 <- (dflist[which(dflist[,"date"] == d & dflist[,"siteID"] == s),"time"])
     
+  tm2 <- lubridate::hms(tm2$time) # turn the true time into a character
+  
+  
+  # identify  the number of seconds between the start time and sunrise (should remain constant)
+  dif_sec_all <- time_length(tm2 - tm)
+  dif_sec_neg <- dif_sec_all[dif_sec_all < 0]
+  dif_sec <- max(dif_sec_neg)
+
+ ## We want the clip to start 5040 seconds from the beginnig and last 300 seconds 
+  
   ww <- which(dflist[,"date"] == d & dflist[,"time"] == t & dflist[,"siteID"] == s)
 
     flp = as.character(list_files_wetlands[ww,"file_path"])
@@ -37,3 +44,8 @@ tmp <- wav_name(d = "20220618")
 
 
 tmp[["flp"]]
+
+ss_5m <- ss_dat$Sunset$SunsetSelection |> filter(Length == "5min") |> 
+  mutate(length_clip = 300,
+         WildTraxName = glue::glue("{SiteID}_{yyyymmdd}_{hhmmss}"),
+         SunRisSet="Sunset")
